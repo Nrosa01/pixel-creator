3289966c7b15413b07df36c0c88badf4c0ed7834
Rioni <nrosa01@ucm.es>
2024-05-01 16:27:38 +0200

    changed vite config again because workflow

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,8 +3,11 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const base = process.env.VITE_BASE ?? '/'
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: base,
   plugins: [vue()],
   resolve: {
     alias: {
