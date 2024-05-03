03c463aee8c18285cc38f4733eea38707837cd69
Rioni <nrosa01@ucm.es>
2024-05-03 03:58:37 +0200

    BlockDialog now fills empty input properly

--- src/components/BlockDialog.vue
+++ src/components/BlockDialog.vue
@@ -34,7 +34,7 @@ function showModal() {
 }
 
 function onModalClose() {
-    play({ playbackRate: 1.5 });   
+    play({ playbackRate: 1.5 });
 
     // Focus is gained when tabbed on the dialog and when this component is created (mounted)
     // So we have to make absolutely sure the focus is in the main workspace or everything will break
@@ -49,7 +49,7 @@ onMounted(() => {
     // We need to save the main workspace to restore it later
     // Because creating the new workspaces will change the focus and thus, the call to Blockly.getMainWorkspace()
     blocklyMainWorkspace = Blockly.getMainWorkspace();
-    
+
     // Windows click eventlistener
     window.addEventListener("pointerup", pointerup);
     window.addEventListener("pointerdown", pointerdown);
@@ -83,11 +83,42 @@ onMounted(() => {
         newBlock.initSvg();
         newBlock.render();
 
+        const getTypeFromConnectionCheck = (connection) => {
+            switch (connection) {
+                case "Particle":
+                    return "particle";
+                case "Vector":
+                    return "direction";
+                case "Number":
+                    return "custom_field_slider";
+                case "Boolean":
+                    return "boolean";
+                case "Group":
+                    return "group_particle";
+                default:
+                    return undefined;
+            }
+        };
+        
+        if (block.inputs !== undefined) {
+            // Iterate over all inputs
+            for (const [key, value] of Object.entries(block.inputs)) {
+                let input = newBlock.getInput(key);
+                if (input.connection.check === null || input.connection.check.length === 0) continue;
+
+                let childBlock = ws.newBlock(getTypeFromConnectionCheck(input.connection.check[0])); // Even if there are multiple checks, we only need the first one
+                childBlock.initSvg();
+                childBlock.render();
+
+                input.connection.connect(childBlock.outputConnection);
+            }
+        }
+
         workspaces.value.push(ws);
         ws.zoomToFit();
     }
 
-     // By doing this we avoid the focus being in the last created workspace.
+    // By doing this we avoid the focus being in the last created workspace.
     // Every workspace div container has class pointer-events-none, so they can't be focused by clicking
     blocklyMainWorkspace.markFocused();
 });
@@ -121,7 +152,9 @@ function pointerup(event) {
         class="backdrop:backdrop-blur-sm backdrop:backdrop-brightness-75 rounded-xl px-4 py-1">
         <div ref="modalInner" class="w-[90vmin] lg:max-h-[90vmin] max-h-[90vh] flex flex-col">
             <form method="dialog">
-                <button class="w-full bg-slate-200 border-2 border-black  hover:bg-slate-300 hover:scale-[1.01]  font-semibold py-1 px-2 rounded sm:grow-0 grow mt-4"><i class="ph-duotone ph-x-circle"></i>Close</button>
+                <button
+                    class="w-full bg-slate-200 border-2 border-black  hover:bg-slate-300 hover:scale-[1.01]  font-semibold py-1 px-2 rounded sm:grow-0 grow mt-4"><i
+                        class="ph-duotone ph-x-circle"></i>Close</button>
             </form>
             <!-- We need pointer events none to truly avoid these workspaces getting focus -->
             <div class="w-full h-full pointer-events-none" v-for="block in toolbox.contents" :key="block.type">
@@ -129,7 +162,9 @@ function pointerup(event) {
                 <p class="mb-8">{{ helpJSON[block.type] }}</p>
             </div>
             <form method="dialog">
-                <button class="w-full bg-slate-200 border-2 border-black  hover:bg-slate-300 hover:scale-[1.01]  font-semibold py-1 px-2 rounded sm:grow-0 grow mb-4"><i class="ph-duotone ph-x-circle"></i>Close</button>
+                <button
+                    class="w-full bg-slate-200 border-2 border-black  hover:bg-slate-300 hover:scale-[1.01]  font-semibold py-1 px-2 rounded sm:grow-0 grow mb-4"><i
+                        class="ph-duotone ph-x-circle"></i>Close</button>
             </form>
         </div>
     </dialog>
