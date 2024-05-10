0fdcbe07943cb355f8c4b69c53f98ed7b745808e
Rioni <nrosa01@ucm.es>
2024-05-10 18:58:57 +0200

    Dependency updted

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,3 +1,5 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -7,6 +9,11 @@ const base = process.env.VITE_BASE ?? '/'
 export default defineConfig({
   base: base,
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
   defineConfig: {
     optimizeDeps: {
       exclude: ['vue-demi']
