0395742213ac6718638527ebd41321c83f989987
Rioni <nrosa01@ucm.es>
2024-05-10 18:40:15 +0200

    Web build

--- src/App.vue
+++
@@ -1,8 +0,0 @@
-<script setup>
-import Loader from "./components/Loader.vue";
-import Simulation from "./components/Simulation.vue";
-</script>
-
-<template>
-  <Loader />
-</template>
--- src/assets/wasm_helpers/plugin.js
+++
@@ -1,8 +0,0 @@
-
-register_plugin = function (importObject) {
-    importObject.env.send_to_js = function (js_object) {
-        console.log(consume_js_object(js_object));
-    }
-}
-
-miniquad_add_plugin({ register_plugin, version: "1.0.0", name: "pixel_creator_api" });
